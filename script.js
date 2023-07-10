8152b14db877d7dad476ebd36769d6fcfc74e184
Moringa School <chepfaith18@gmail.com>
2023-07-10 10:13:37 +0300

    Getting the recipe close button element

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const searchBtn = document.getElementById('search-btn');
 // Get the meal list element
 const mealList = document.getElementById('meal');
 
-
 // Get the meal details content element
 const mealDetailsContent = document.querySelector('.meal-details-content');
+
+// Get the recipe close button element
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
