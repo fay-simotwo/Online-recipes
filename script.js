d88bb83faa9850d376cb947c9d881eb0f312ccef
Moringa School <chepfaith18@gmail.com>
2023-07-10 10:12:46 +0300

    Getting the meal details content element

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const searchBtn = document.getElementById('search-btn');
 
 // Get the meal list element
 const mealList = document.getElementById('meal');
+
+
+// Get the meal details content element
+const mealDetailsContent = document.querySelector('.meal-details-content');
